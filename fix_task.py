526c333363d386661f0d5b7cf13d6e206fff01a0
Hanzhi <yinhanzhi@gmail.com>
2023-03-28 07:43:44 -0400

    factor parameter to another class

--- fix_task.py
+++ fix_task.py
@@ -1,6 +1,4 @@
 from collections import defaultdict
-from dataclasses import dataclass
-from pathlib import Path
 from pprint import pprint
 from typing import get_args
 
@@ -9,7 +7,6 @@ import torch
 import torch.nn as nn
 import wandb
 from matplotlib.figure import Figure
-from rootconfig import RootConfig
 from torch import Tensor
 from torch.cuda import is_available as cuda_is_available
 from torch.cuda.amp.grad_scaler import GradScaler
@@ -23,47 +20,16 @@ from src.dataset import FixDataset, download_signal_train_dataset_to
 from src.evaluation import (evaluate_rms_difference,
                             evaluate_waveform_difference)
 from src.loss import LossType, forge_loss_function_from
-from src.model import Activation, DRCSideChainModelVersion, forge_drc_model_by
+from src.model import forge_drc_model_by
+from src.parameter import FixTaskParameter
 from src.utils import clear_memory, current_utc_time, set_random_seed_to
 
 if __name__ != '__main__':
     raise RuntimeError(f'The main script cannot be imported by other module.')
 
 
-@dataclass
-class Parameter(RootConfig):
-    random_seed: int = 42
-
-    dataset_dir: Path = Path('./data/SignalTrain')
-    data_segment_length: float = 1.0
-
-    epoch: int = 50
-    learning_rate: float = 1e-3
-    s4_learning_rate: float = 1e-3
-    batch_size: int = 32
-
-    model_version: DRCSideChainModelVersion = 1
-    model_inner_audio_channel: int = 16
-    model_s4_hidden_size: int = 16
-    model_activation: Activation = 'GELU'
-    model_depth: int = 6
-    model_take_db: bool = False
-    model_take_abs: bool = False
-    model_take_amp: bool = False
-
-    loss: LossType = 'ESR+DC+Multi-STFT'
-    loss_filter_coef: float = 0.0
-
-    log_wandb: bool = True
-    wandb_entity: str = 'int0thewind'
-    wandb_project_name: str = 'S4 Dynamic Range Compressor'
-
-    save_checkpoint: bool = True
-    checkpoint_dir: Path = Path('./experiment-result')
-
-
 '''Script parameters.'''
-param = Parameter.parse_args()
+param = FixTaskParameter.parse_args()
 pprint(param.to_dict())
 
 '''The preparatory work.'''
@@ -154,7 +120,7 @@ for epoch in range(param.epoch):
         x: Tensor
         y: Tensor
         if torch.rand(1).item() < 0.5:
-            x, y = invert_phase(x, y)
+            x, y = invert_phase(x, y)  # Data augmentation
         x = x.to(device)
         y = y.to(device)
 
---
+++ src/parameter.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from rootconfig import RootConfig
+
+from .loss import LossType
+from .model import Activation, DRCSideChainModelVersion
+
+
+@dataclass
+class FixTaskParameter(RootConfig):
+    random_seed: int = 42
+
+    dataset_dir: Path = Path('./data/SignalTrain')
+    data_segment_length: float = 1.0
+
+    epoch: int = 50
+    learning_rate: float = 1e-3
+    s4_learning_rate: float = 1e-3
+    batch_size: int = 32
+
+    model_version: DRCSideChainModelVersion = 1
+    model_inner_audio_channel: int = 16
+    model_s4_hidden_size: int = 16
+    model_activation: Activation = 'GELU'
+    model_depth: int = 6
+    model_take_db: bool = False
+    model_take_abs: bool = False
+    model_take_amp: bool = False
+
+    loss: LossType = 'ESR+DC+Multi-STFT'
+    loss_filter_coef: float = 0.0
+
+    log_wandb: bool = True
+    wandb_entity: str = 'int0thewind'
+    wandb_project_name: str = 'S4 Dynamic Range Compressor'
+
+    save_checkpoint: bool = True
+    checkpoint_dir: Path = Path('./experiment-result')

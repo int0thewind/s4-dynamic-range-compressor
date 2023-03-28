47d677a7b1d3117f91461f0fa3b0c2fc41bf1ad4
Hanzhi <yinhanzhi@gmail.com>
2023-03-27 21:23:53 -0400

    fix some model name

--- src/model.py
+++ src/model.py
@@ -9,11 +9,11 @@ from torch import Tensor
 from .layer import DSSM, Absolute, Amplitude, Decibel, Rearrange
 
 __all__ = [
-    'Activation', 'DRCModelVersion', 'forge_drc_model_by',
+    'Activation', 'DRCSideChainModelVersion', 'forge_drc_model_by',
 ]
 
 Activation = Literal['tanh', 'sigmoid', 'GELU', 'ReLU', 'Identity']
-DRCModelVersion = Literal[0, 1, 2]
+DRCSideChainModelVersion = Literal[0, 1, 2, 3]
 
 
 def get_activation_type_from(activation: Activation) -> Type[nn.Module]:
@@ -80,7 +80,7 @@ class AbstractDRCSideChainModel(ABC, nn.Module):
         return x * self.side_chain(x)
 
 
-class DRCModelV0(AbstractDRCSideChainModel):
+class DRCSideChainModelV0(AbstractDRCSideChainModel):
     @staticmethod
     def forge_layer_sequence(
         inner_audio_channel: int, s4_hidden_size: int,
@@ -109,7 +109,7 @@ class DRCModelV0(AbstractDRCSideChainModel):
         return layers
 
 
-class DRCModelV1(AbstractDRCSideChainModel):
+class DRCSideChainModelV1(AbstractDRCSideChainModel):
     @staticmethod
     def forge_layer_sequence(
         inner_audio_channel: int, s4_hidden_size: int,
@@ -144,7 +144,42 @@ class DRCModelV1(AbstractDRCSideChainModel):
         return layers
 
 
-class DRCModelV2(AbstractDRCSideChainModel):
+class DRCSideChainModelV2(AbstractDRCSideChainModel):
+    @staticmethod
+    def forge_layer_sequence(
+        inner_audio_channel: int, s4_hidden_size: int,
+        s4_learning_rate: float | None,
+        model_depth: int, activation: Activation
+    ) -> list[nn.Module]:
+        layers: list[nn.Module] = []
+
+        Act = get_activation_type_from(activation)
+
+        layers.extend([
+            Rearrange('B L  -> B L 1'),
+            nn.Linear(1, inner_audio_channel),
+            Rearrange('B L H -> B H L'),
+            DSSM(inner_audio_channel, s4_hidden_size, lr=s4_learning_rate),
+            Rearrange('B H L -> B L H'),
+            Act(),
+        ])
+        for _ in range(model_depth):
+            layers.extend([
+                nn.Linear(inner_audio_channel, inner_audio_channel),
+                Rearrange('B L H -> B H L'),
+                DSSM(inner_audio_channel, s4_hidden_size, lr=s4_learning_rate),
+                Rearrange('B H L -> B L H'),
+                Act(),
+            ])
+        layers.extend([
+            nn.Linear(inner_audio_channel, 1),
+            Rearrange('B L 1 -> B L')
+        ])
+
+        return layers
+
+
+class DRCSideChainModelV3(AbstractDRCSideChainModel):
     @staticmethod
     def forge_layer_sequence(
         inner_audio_channel: int, s4_hidden_size: int,
@@ -182,27 +217,27 @@ class DRCModelV2(AbstractDRCSideChainModel):
 
 
 def forge_drc_model_by(
-    model_version: DRCModelVersion,
+    model_version: DRCSideChainModelVersion,
     inner_audio_channel: int, s4_hidden_size: int,
     s4_learning_rate: float | None,
     model_depth: int, activation: Activation,
     take_db: bool, take_abs: bool, take_amp: bool,
 ) -> AbstractDRCSideChainModel:
     if model_version == 1:
-        return DRCModelV1(
+        return DRCSideChainModelV1(
             inner_audio_channel, s4_hidden_size,
             s4_learning_rate,
             model_depth, activation,
             take_db, take_abs, take_amp,
         )
     if model_version == 2:
-        return DRCModelV2(
+        return DRCSideChainModelV3(
             inner_audio_channel, s4_hidden_size,
             s4_learning_rate,
             model_depth, activation,
             take_db, take_abs, take_amp,
         )
-    return DRCModelV0(
+    return DRCSideChainModelV0(
         inner_audio_channel, s4_hidden_size,
         s4_learning_rate,
         model_depth, activation,

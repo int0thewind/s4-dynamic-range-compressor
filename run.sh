54aaa2309bb0c472932f2587ba316601fde1a52e
Hanzhi <yinhanzhi@gmail.com>
2023-08-03 10:59:45 -0400

    make shell script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-pipenv run python3 -m s4drc fit --trainer.accelerator cpu --trainer.default_root_dir ./experiment-result --trainer.max_epochs 100 --trainer.logger WandbLogger --trainer.logger.init_args.project S4DRC --trainer.logger.init_args.log_model all
+pipenv run python3 -m s4drc fit --trainer.default_root_dir ./experiment-result --trainer.max_epochs 100 --trainer.logger WandbLogger --trainer.logger.init_args.project S4DRC --trainer.logger.init_args.log_model all
